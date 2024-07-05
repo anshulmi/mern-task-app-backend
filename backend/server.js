7a1cb8371b16295fe2786e24421a4c05e7177dde
Anshul Mishra <anshulmi191@gmail.com>
2024-07-06 01:01:10 +0530

    changed fronend url

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended:false}))
 app.use(cors({
-    origin: ["http://localhost:3000","https://mern-task-app.orrender.com"]
+    origin: ["http://localhost:3000","https://frontend-u6bt.orrender.com"]
 }));
 app.use("/api/tasks",taskRouter)
 
