72dc87b8988040e101493ed0b1273756217e4dce
Anshul Mishra <anshulmi191@gmail.com>
2024-07-06 01:07:48 +0530

    changed frontend url

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended:false}))
 app.use(cors({
-    origin: ["http://localhost:3000","https://frontend-u6bt.orrender.com"]
+    origin: ["http://localhost:3000","https://frontend-u6bt.onrender.com"]
 }));
 app.use("/api/tasks",taskRouter)
 
